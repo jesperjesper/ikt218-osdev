7a53c27493940bcc40486f2a614e5d8eb4a3156c
jespereh <jespereh@uia.no>
2023-05-29 19:06:55 +0200

    Added gdt portion (maybe)

---
+++ group_13/src/kernel/src/gdt/gdt.cpp
@@ -0,0 +1,45 @@
+#include "gdt.h"
+#include <stdint.h>
+#include "system.h"
+
+
+struct gdt_entry gdt_entries[GDT_ENTRIES];
+struct gdt_ptr ptr_gdt;
+
+//This allows us to use the flush function we made in our assembly file
+extern "C" {
+    extern void gdt_flush(uint32_t);
+}
+
+
+
+
+// Set the value of one GDT entry.
+static void gdt_set_gate(s32int num, u32int base, u32int limit, u8int access, u8int gran)
+{
+   gdt_entries[num].base_low    = (base & 0xFFFF);
+   gdt_entries[num].base_middle = (base >> 16) & 0xFF;
+   gdt_entries[num].base_high   = (base >> 24) & 0xFF;
+
+   gdt_entries[num].limit_low   = (limit & 0xFFFF);
+   gdt_entries[num].granularity = (limit >> 16) & 0x0F;
+
+   gdt_entries[num].granularity |= gran & 0xF0;
+   gdt_entries[num].access      = access;
+} 
+
+
+void init_gdt()
+{
+    ptr.gdt.limit = sizeof(struct gdt_entry) * GDT_ENTRIES - 1;
+    ptr.gdt.base = (uint32_t)&gdt_entries;
+
+    gdt_set_gate(0, 0, 0, 0, 0);                // Null segment
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment
+    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment
+    gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment
+
+   
+    gdt_flush((uint32_t)&gdt_ptr);
+}
--- group_13/src/kernel/src/main.cpp
+++ group_13/src/kernel/src/main.cpp
@@ -1,6 +1,6 @@
 
 #include "system.h"
-
+#include "gdt.h"
 // Define entry point in asm to prevent C++ mangling
 extern "C"{
     void kernel_main();
