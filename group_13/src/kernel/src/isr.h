19a509a29e7f6018373ccd5abd7f109e7f5ffe4b
jespereh <jespereh@uia.no>
2023-06-02 16:35:52 +0200

    111

---
+++ group_13/src/kernel/src/isr.h
@@ -0,0 +1,89 @@
+#ifndef ISR_H
+#define ISR_H
+#include "idt.h"
+
+#define ISR1 1
+#define ISR2 2
+#define ISR3 3
+#define ISR4 4
+#define ISR5 5
+#define ISR6 6
+#define ISR7 7
+#define ISR8 8
+#define ISR9 9
+#define ISR10 10
+#define ISR11 11
+#define ISR12 12
+#define ISR13 13
+#define ISR14 14
+#define ISR15 15
+#define ISR16 16
+#define ISR17 17
+#define ISR18 18
+#define ISR19 19
+#define ISR20 20
+#define ISR21 21
+#define ISR22 22
+#define ISR23 23
+#define ISR24 24
+#define ISR25 25
+#define ISR26 26
+#define ISR27 27
+#define ISR28 28
+#define ISR29 29
+#define ISR30 30
+#define ISR31 31
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+
+
+typedef struct registers
+  {
+      uint32_t ds;               
+      uint32_t edi, esi, ebp, useless_value, ebx, edx, ecx, eax; 
+      uint32_t int_no, err_code;    
+      uint32_t eip, cs, eflags, esp, ss; 
+  } registers_t;
+
+
+typedef struct int_handler_t 
+  {
+  int num;
+  isr_t handler;
+  void *data;
+};
+
+
+typedef void (*isr_t)(registers_t);
+
+void register_interrupt_handler(uint8_t n, isr_t handler);
